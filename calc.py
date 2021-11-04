38fde0bfab28bddd43ff4db82af3ffb983b6bcc8
wm1511 <wiktor.merta@op.pl>
2021-11-04 19:34:31 +0100

    Optimized, new theme

--- calc.py
+++ calc.py
@@ -3,46 +3,6 @@ import scipy.io.wavfile
 import ui
 
 
-def clear_array(array, silence_length):
-    part_arrays = []
-    begin = array[0]
-    current = 0
-    length = 0
-    for el in array:
-        if current + 1 < array.size:
-            if el + 1 == array[current + 1]:
-                length += 1
-                if length > silence_length:
-                    part_arrays.append(array[begin:current])
-                    length = 0
-                    begin = current + 1
-            else:
-                length = 0
-                begin = current + 1
-            current += 1
-    return np.concatenate(part_arrays)
-
-
-def indices_to_values(index_array, value_array):
-    result_array = np.zeros((value_array.size,), dtype=float)
-    for index in index_array:
-        result_array[index] = value_array[index]
-    return result_array
-
-
-def replace_arrays(source_array, silence_array):
-    current = 0
-    added = 0
-    result_array = np.empty((source_array.size - np.count_nonzero(silence_array),))
-    for sample in silence_array:
-        if sample == 0:
-            result_array[added] = source_array[current]
-            added += 1
-        current += 1
-    return result_array
-
-
-# TODO Naprawić reshape
 class SoundWave:
     def __init__(self, path):
         try:
@@ -50,6 +10,7 @@ class SoundWave:
         except ValueError as e:
             print(e)
             ui.data_msb()
+        self.wa = None
         self.na = None
         self.sa = None
 
@@ -58,16 +19,13 @@ class SoundWave:
         self.reshape_array(silence_length)
         self.get_silence(trim_level)
 
-    def cut_silence(self):
-        self.y = self.na
-
     def reshape_array(self, silence_length):
         if len(np.shape(self.y)) == 1:
             pad_front = int((silence_length - (self.y.size % silence_length)) / 2)
             pad_end = int((silence_length - (self.y.size % silence_length)) / 2) \
                 if self.y.size % 2 == 0 else int((silence_length - (self.y.size % silence_length)) / 2) + 1
             new_length = int((self.y.size + pad_front + pad_end) / silence_length)
-            self.y = np.reshape(np.pad(self.y, (pad_front, pad_end)), (new_length, silence_length))
+            self.wa = np.reshape(np.pad(self.y, (pad_front, pad_end)), (new_length, silence_length))
         elif len(np.shape(self.y)) == 2:
             pad_front = int((silence_length - (self.y[:, 0].size % silence_length)) / 2)
             pad_end = int((silence_length - (self.y[:, 0].size % silence_length)) / 2) \
@@ -75,76 +33,35 @@ class SoundWave:
             new_length = int((self.y[:, 0].size + pad_front + pad_end) / silence_length)
             padded = np.row_stack((np.pad(self.y[:, 0], (pad_front, pad_end)),
                                    np.pad(self.y[:, 1], (pad_front, pad_end))))
-            self.y = np.reshape(padded, (len(np.shape(self.y)), new_length, silence_length))
+            self.wa = np.reshape(padded, (len(np.shape(self.y)), new_length, silence_length))
 
     def get_silence(self, trim_level):
         cut_arrays = []
-        silence_array = np.zeros(self.y.shape)
-        if len(np.shape(self.y)) == 2:
-            for i in range(self.y.shape[0]):
-                if np.abs(np.amax(self.y[i, :])) < trim_level:
-                    silence_array[i, :] = self.y[i, :]
+        silence_array = np.zeros(self.wa.shape, dtype=int)
+        if len(np.shape(self.wa)) == 2:
+            for i in range(self.wa.shape[0]):
+                if np.abs(np.amax(self.wa[i, :])) < trim_level:
+                    silence_array[i, :] = self.wa[i, :]
                 else:
-                    cut_arrays.append(self.y[i, :])
-            self.sa = np.reshape(silence_array, (self.y.shape[1] * self.y.shape[2]))
-            self.na = np.reshape(np.hstack(cut_arrays), (len(cut_arrays) * self.y.shape[2]))
-            self.y = np.reshape(self.y, (self.y.shape[1] * self.y.shape[2]))
-        elif len(np.shape(self.y)) == 3:
-            for i in range(self.y.shape[1]):
-                if np.abs(np.amax(self.y[0, i, :])) < trim_level:
-                    silence_array[:, i, :] = self.y[:, i, :]
+                    cut_arrays.append(self.wa[i, :])
+            self.sa = np.reshape(silence_array, (self.wa.shape[1] * self.wa.shape[2]))
+            self.na = np.rot90(np.reshape(np.row_stack(cut_arrays), (len(cut_arrays) * self.wa.shape[2])), k=3)
+        elif len(np.shape(self.wa)) == 3:
+            for i in range(self.wa.shape[1]):
+                if np.abs(np.amax(self.wa[0, i, :])) < trim_level:
+                    silence_array[:, i, :] = self.wa[:, i, :]
                 else:
-                    cut_arrays.append(self.y[:, i, :])
-            self.sa = np.reshape(silence_array, (self.y.shape[1] * self.y.shape[2], self.y.shape[0]))
-            self.na = np.reshape(np.hstack(cut_arrays), (len(cut_arrays) * self.y.shape[2], self.y.shape[0]))
-            self.y = np.reshape(self.y, (self.y.shape[1] * self.y.shape[2], self.y.shape[0]))
-            print(np.hstack(cut_arrays), np.hstack(cut_arrays).shape)
+                    cut_arrays.append(self.wa[:, i, :])
+            self.sa = np.rot90(np.reshape(silence_array, (self.wa.shape[0], self.wa.shape[1] * self.wa.shape[2])), k=3)
+            ch1 = np.row_stack([i[0, :] for i in cut_arrays])
+            ch2 = np.row_stack([i[1, :] for i in cut_arrays])
+            self.na = np.rot90(np.row_stack((np.reshape(ch1, (ch1.shape[0] * ch1.shape[1])),
+                                             np.reshape(ch2, (ch2.shape[0] * ch2.shape[1])))), k=3)
 
     def export_int16(self, path):
         if path != '':
-            scipy.io.wavfile.write(path + '.wav', self.sr, self.y.astype(np.int16))
+            scipy.io.wavfile.write(path + '.wav', self.sr, self.na.astype(np.int16))
 
     def export_int32(self, path):
         if path != '':
-            scipy.io.wavfile.write(path + '.wav', self.sr, self.y.astype(np.int32))
-
-    def detect_silence_old(self, top, silence_length):
-        trim_level = np.amax(self.y) * (top / 100)
-
-        if len(np.shape(self.y)) == 1:
-            silent_indices = clear_array(np.where(abs(self.y) < trim_level)[0], silence_length)
-            self.sa = indices_to_values(silent_indices, self.y)
-
-        elif len(np.shape(self.y)) == 2:
-            channel1 = clear_array(np.where(abs(self.y[:, 0]) < trim_level)[0], silence_length)
-            channel2 = clear_array(np.where(abs(self.y[:, 1]) < trim_level)[0], silence_length)
-            if channel1.size > channel2.size:
-                delta = channel1.size - channel2.size
-                channel2 = np.pad(channel2, (0, int(delta)), 'symmetric')
-            elif channel2.size > channel1.size:
-                delta = channel2.size - channel1.size
-                channel1 = np.pad(channel1, (0, int(delta)), 'symmetric')
-            channel1_silence = indices_to_values(channel1, self.y[:, 0])
-            channel2_silence = indices_to_values(channel2, self.y[:, 1])
-            self.sa = np.column_stack((channel1_silence, channel2_silence))
-
-        else:
-            ui.channel_msb()
-
-    def cut_silence_old(self):
-        if len(np.shape(self.y)) == 1:
-            self.y = replace_arrays(self.y, self.sa)
-
-        elif len(np.shape(self.y)) == 2:
-            channel1 = replace_arrays(self.y[:, 0], self.sa[:, 0])
-            channel2 = replace_arrays(self.y[:, 1], self.sa[:, 1])
-            if channel1.size > channel2.size:
-                delta = channel1.size - channel2.size
-                channel2 = np.pad(channel2, (0, int(delta)), 'symmetric')
-            elif channel2.size > channel1.size:
-                delta = channel2.size - channel1.size
-                channel1 = np.pad(channel1, (0, int(delta)), 'symmetric')
-            self.y = np.column_stack((channel1, channel2))
-
-        else:
-            ui.channel_msb()
+            scipy.io.wavfile.write(path + '.wav', self.sr, self.na.astype(np.int32))
--- ui.py
+++ ui.py
@@ -1,8 +1,9 @@
 import tkinter as tk
-import tkinter.ttk
+import tkinter.ttk as ttk
 import tkinter.messagebox
 import tkinter.filedialog
 import matplotlib.pyplot as plt
+from ttkthemes import ThemedTk
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 import numpy as np
 import calc
@@ -35,10 +36,6 @@ def plot(array, sr, color):
     plt.ylabel('Amplitude')
 
 
-def only_numbers(char):
-    return char.isdigit()
-
-
 def channel_msb():
     tk.messagebox.showerror(title="File error", message="Incorrect count of audio channels!")
 
@@ -50,63 +47,62 @@ def data_msb():
 class App:
     def __init__(self):
         self.wave = None
-        self.root = tk.Tk()
+        self.root = ThemedTk(theme='arc', themebg=True)
         self.root.title('Silence trimmer')
         fig = plt.figure(figsize=(15, 5))
         self.canvas = FigureCanvasTkAgg(fig, master=self.root)
         self.canvas.get_tk_widget().grid(row=2, columnspan=5)
 
-        misc_frame = tk.Frame(self.root)
-        open_button = tk.Button(misc_frame, text='Open file', command=self.open_file)
+        misc_frame = ttk.Frame(self.root)
+        open_button = ttk.Button(misc_frame, text='Open file', command=self.open_file)
         open_button.grid(row=0, pady=5)
-        save_button = tk.Button(misc_frame, text='Save file', command=self.export_file)
+        save_button = ttk.Button(misc_frame, text='Save file', command=self.export_file)
         save_button.grid(row=1, pady=5)
-        exit_button = tk.Button(misc_frame, text='Exit', command=self.root.quit)
+        exit_button = ttk.Button(misc_frame, text='Exit', command=self.root.quit)
         exit_button.grid(row=2, pady=5)
         misc_frame.grid(column=0, row=4, padx=50, pady=20, sticky=tk.W)
 
-        plot_frame = tk.Frame(self.root)
-        fresh_button = tk.Button(plot_frame, text='Plot source wave', command=self.plot_fresh)
+        plot_frame = ttk.Frame(self.root)
+        fresh_button = ttk.Button(plot_frame, text='Plot source wave', command=self.plot_fresh)
         fresh_button.grid(row=0, pady=5)
-        silence_button = tk.Button(plot_frame, text='Plot silence', command=self.plot_silence)
-        silence_button.grid(row=1, pady=5)
-        cut_button = tk.Button(plot_frame, text='Cut silence', command=self.cut_silence)
-        cut_button.grid(row=2, pady=5)
+        cut_button = ttk.Button(plot_frame, text='Plot cut wave', command=self.cut_silence)
+        cut_button.grid(row=1, pady=5)
         plot_frame.grid(column=4, row=4, padx=50, pady=20, sticky=tk.E)
 
-        top_frame = tk.Frame(self.root)
-        top_label = tk.Label(top_frame, text='% of sound amplitude considered as silence')
+        top_frame = ttk.Frame(self.root)
+        top_label = ttk.Label(top_frame, text='% of sound amplitude considered as silence')
         top_label.grid()
-        self.top_spinbox = tk.Spinbox(top_frame, from_=1, to=99)
+        self.top_spinbox = ttk.Spinbox(top_frame, from_=1, to=99)
         self.top_spinbox.delete(0)
         self.top_spinbox.insert(0, 5)
         self.top_spinbox.grid(row=1, pady=5)
         top_frame.grid(column=3, row=4, padx=50, pady=20)
 
-        top_button = tk.Button(self.root, text='Detect silence', command=self.initialize_detection)
-        top_button.grid(column=2, row=4, pady=5)
+        detect_button = ttk.Button(self.root, text='Detect silence', command=self.initialize_detection)
+        detect_button.grid(column=2, row=4, pady=5)
 
-        chunk_frame = tk.Frame(self.root)
-        chunk_label = tk.Label(chunk_frame, text='Minimal duration of silence chunk in ms')
+        chunk_frame = ttk.Frame(self.root)
+        chunk_label = ttk.Label(chunk_frame, text='Minimal duration of silence chunk in ms')
         chunk_label.grid()
-        self.chunk_spinbox = tk.Spinbox(chunk_frame, from_=1, to=1000)
+        self.chunk_spinbox = ttk.Spinbox(chunk_frame, from_=1, to=1000)
         self.chunk_spinbox.delete(0)
         self.chunk_spinbox.insert(0, 100)
         self.chunk_spinbox.grid(row=1, pady=5)
         chunk_frame.grid(column=1, row=4, padx=50, pady=20)
 
-        toolbar_frame = tk.Frame(self.root)
+        toolbar_frame = ttk.Frame(self.root)
         toolbar = NavigationToolbar2Tk(self.canvas, toolbar_frame)
         toolbar.update()
         toolbar_frame.grid(row=0, pady=20, columnspan=5)
 
-        tkinter.ttk.Separator(self.root, orient=tk.HORIZONTAL).grid(column=0, row=1, columnspan=5, sticky='ew')
-        tkinter.ttk.Separator(self.root, orient=tk.HORIZONTAL).grid(column=0, row=3, columnspan=5, sticky='ew')
+        ttk.Separator(self.root, orient=tk.HORIZONTAL).grid(column=0, row=1, columnspan=5, sticky='ew')
+        ttk.Separator(self.root, orient=tk.HORIZONTAL).grid(column=0, row=3, columnspan=5, sticky='ew')
 
         self.root.mainloop()
 
     def plot_fresh(self):
         try:
+            self.plot_clear()
             plot(self.wave.y, self.wave.sr, 'blue')
             self.canvas.draw()
         except AttributeError:
@@ -120,8 +116,12 @@ class App:
             tk.messagebox.showerror(title="File error", message="You have to open sound file first!")
 
     def plot_cut(self):
-        plot(self.wave.y, self.wave.sr, 'green')
-        self.canvas.draw()
+        try:
+            self.plot_clear()
+            plot(self.wave.na, self.wave.sr, 'green')
+            self.canvas.draw()
+        except AttributeError:
+            tk.messagebox.showerror(title="File error", message="You have to cut sound first!")
 
     def plot_clear(self):
         plt.clf()
@@ -129,16 +129,17 @@ class App:
 
     def initialize_detection(self):
         try:
+            self.plot_clear()
             self.wave.detect_silence(int(self.top_spinbox.get()),
                                      int(int(self.chunk_spinbox.get()) * (self.wave.sr / 1000)))
+            self.plot_fresh()
+            self.plot_silence()
         except AttributeError as e:
             print(e)
             tk.messagebox.showerror(title="File error", message="You have to open sound file first!")
 
     def cut_silence(self):
         try:
-            self.wave.cut_silence()
-            self.plot_clear()
             self.plot_cut()
         except AttributeError:
             tk.messagebox.showerror(title="File error", message="You have to open sound file first!")
@@ -153,9 +154,9 @@ class App:
 
     def export_file(self):
         try:
-            if np.amax(self.wave.y) > np.iinfo(np.int16).max:
+            if np.amax(self.wave.na) > np.iinfo(np.int16).max:
                 self.wave.export_int32(file_save())
-            elif np.amax(self.wave.y) <= np.iinfo(np.int16).max:
+            elif np.amax(self.wave.na) <= np.iinfo(np.int16).max:
                 self.wave.export_int16(file_save())
             else:
                 tk.messagebox.showerror(title="File error", message="Incorrect data!")
